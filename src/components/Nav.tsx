3f363bed2af5272a4385ba68e93c9dd2fc75434c
johnspencerp <johnspencerp@gmail.com>
2026-03-28 13:22:05 -0500

    fix: check showSubscriptions (not showMemberships) for membership link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,7 +12,7 @@ export default function Nav() {
     showSocial: false,
     showEvents: false,
     showNewArrivals: false,
-    showMemberships: false,
+    showSubscriptions: false,
   });
 
   useEffect(() => {
@@ -37,7 +37,7 @@ export default function Nav() {
           {nav.showBookings && storeInfo.bookingsEnabled && (
             <Link href="/bookings" className="hover:underline">Book</Link>
           )}
-          {nav.showMemberships && (
+          {nav.showSubscriptions && (
             <Link href="/membership" className="hover:underline">Membership</Link>
           )}
           {nav.showEvents && (
